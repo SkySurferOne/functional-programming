45355b3f75264dd10804db1efe8cbd844875e4b2
SkySurferOne <skysurfer95@interia.pl>
2016-11-25 13:08:55 +0100

    add part of lab3 ex

--- lab3/ex1.hs
+++ lab3/ex1.hs
@@ -10,7 +10,6 @@ div2_ = \x -> 2 / x
 _div4 = \x -> x / 3
 substr = \x -> 4 - x
 
-
 f7 x = if x `mod` 2 == 0 then True else False
 f7a = \x -> if x `mod` 2 == 0 then True else False
 
@@ -28,3 +27,55 @@ sum' (x:xs) = x + sum' xs
 sumSqr' :: Num a => [a] -> a
 sumSqr' [] = 0
 sumSqr' (x:xs) = x ^ 2 + sumSqr' xs
+
+sumWith :: Num a => (a -> a) -> [a] -> a
+sumWith f [] = 0
+sumWith f (x:[]) = f x
+sumWith f (x:xs) = (f x) + sumWith f xs
+
+sum     = sumWith id
+sumSqr  = sumWith (\y -> y ^ 2)
+sumCube = sumWith (\y -> y ^ 3)
+sumAbs  = sumWith abs
+
+listLength = sumWith (\y -> 1)
+
+sqr x = x^2
+
+funcFactory n = case n of
+ 1 -> id
+ 2 -> sqr
+ 3 -> (^3)
+ 4 -> \x -> x^4
+ 5 -> intFunc
+ _ -> const n
+ where
+   intFunc x = x^5
+
+factorial :: Double -> Double
+factorial n | n == 0 = 1
+            | otherwise = n * factorial (n-1)
+
+sumWith2 :: (Num a, Floating b) => (a -> b -> b) -> [a] -> b -> b
+sumWith2 f [] x = 0
+sumWith2 f (k:[]) x = f k x
+sumWith2 f (k:ks) x = (f k x) + sumWith2 f ks x
+
+expApproxUpTo :: Int -> Double -> Double
+expApproxUpTo n = sumWith2 (\k x -> x ^ k / factorial (fromIntegral k)) [0..n]
+
+dfr :: (Double -> Double) -> Double -> (Double -> Double)
+dfr f h = ff f h where ff f h x = (f (x + h) - f x) / h
+
+funcList :: [ Double -> Double ]
+funcList = [ \x -> (sin x)/x, \x -> log x + sqrt x + 1, \x -> (exp 1) ** x]
+
+evalFuncListAt :: a -> [a -> b] -> [b]
+evalFuncListAt x [] = []
+evalFuncListAt x (f:fs) = f x : evalFuncListAt x fs
+
+displEqs :: (Double -> Double, Double -> Double)
+displEqs = (\t -> 4 * t^2 + 2 * t, \t -> 3 * t^2)
+
+funcListExt :: [ Double -> Double ]
+funcListExt = [ \x -> (sin x)/x, \x -> log x + sqrt x + 1, \x -> (exp 1) ** x, \t -> sqrt (1 + t)]
